6d8b44be4c47e0e566398d38938fe1c459a8f045
Mattia Di Fatta <mattiadf09@gmail.com>
2019-04-11 22:05:39 -0500

    Minor changes

--- source/video_handler/frames_3d_extractor.py
+++ source/video_handler/frames_3d_extractor.py
@@ -1,13 +1,15 @@
 import cv2
 
+
 def main():
     print('starting main ...')
     cap = cv2.VideoCapture('../../data/videos/case-1-3D.avi')
+    cap.set(cv2.CAP_PROP_POS_MSEC, 17000)
     i = 0
     while cap.isOpened():
         ret, f = cap.read()
         if ret:
-            cv2.imwrite("../..data/datasets/full3d/frame-%d.png" % i, f)
+            cv2.imwrite("../../data/datasets/full3d/frame-%d.png" % i, f)
             i = i + 1
         else:
             break
