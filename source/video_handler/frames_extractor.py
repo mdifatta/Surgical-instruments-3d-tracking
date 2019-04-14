ecb5a3a310358f583eeae18f6bd7b0c69817c633
Mattia Di Fatta <mattia.difatta@mail.polimi.it>
2019-04-14 11:15:58 -0500

    Changes frames' step

--- source/video_handler/frames_extractor.py
+++ source/video_handler/frames_extractor.py
@@ -8,13 +8,16 @@ from tqdm import tqdm
 
 def main():
     cap = cv2.VideoCapture('../case-1-2D.avi')
-    # TODO: the index must be consecutive across videos
+    # TODO: the index must be consecutive across videos so first generate frames then
+    # rename frames with consecutive filenames
     id = 0
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
     length = int(length / 10) * 10
 
-    for i in tqdm(np.arange(start=0, step=3, stop=length)):
+    frames_step = 6
+
+    for i in tqdm(np.arange(start=0, step=frames_step, stop=length)):
         cap.set(cv2.CAP_PROP_POS_FRAMES, i)
         ret, frame = cap.read()
 
