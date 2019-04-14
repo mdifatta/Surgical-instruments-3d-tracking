2de2ed31d596851f8f1dc7fe10cef77c6f43649a
Mattia Di Fatta <mattia.difatta@mail.polimi.it>
2019-04-14 12:26:53 -0500

    Generalized code with tunable params

--- source/video_handler/frames_extractor.py
+++ source/video_handler/frames_extractor.py
@@ -7,22 +7,23 @@ from tqdm import tqdm
 
 
 def main():
-    cap = cv2.VideoCapture('../case-1-2D.avi')
+    frames_step = 6
+    video_name = "case-1-2D.avi"
+    folder_name = "video-1"
+
+    cap = cv2.VideoCapture('../%s' % video_name)
     # TODO: the index must be consecutive across videos so first generate frames then
     # rename frames with consecutive filenames
     id = 0
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
     length = int(length / 10) * 10
-
-    frames_step = 6
 
     for i in tqdm(np.arange(start=0, step=frames_step, stop=length)):
         cap.set(cv2.CAP_PROP_POS_FRAMES, i)
         ret, frame = cap.read()
 
         if ret:
-            cv2.imwrite('../case-1/frame%d.png' % id, frame, [cv2.IMWRITE_PNG_COMPRESSION, 5])
+            cv2.imwrite('../%s/frame%d.png' % (folder_name, id), frame, [cv2.IMWRITE_PNG_COMPRESSION, 5])
             id = id + 1
         else:
             print('exiting...')
