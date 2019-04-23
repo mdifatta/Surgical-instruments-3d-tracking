cd9d96400bece28df38ffea80a0ca1343a3c6084
Mattia Di Fatta <mattia.difatta@mail.polimi.it>
2019-04-23 18:03:00 -0500

    Fixed paths

--- source/tool_shadow/neural_networks/dataset_file_gen.py
+++ source/tool_shadow/neural_networks/dataset_file_gen.py
@@ -4,7 +4,7 @@ from sklearn.utils import shuffle
 
 def main():
     # read entire csv
-    df = pd.read_csv('../../data/targets/targets.csv', sep=';')
+    df = pd.read_csv('../../../data/targets/targets.csv', sep=';')
     # shuffle rows
     df = shuffle(df)
     # count rows
@@ -19,9 +19,9 @@ def main():
     train, valid = train[:valid_len], train[valid_len:]
 
     # create new csv files
-    train.to_csv('../../data/targets/train.csv', sep=';', index=False)
-    test.to_csv('../../data/targets/test.csv', sep=';', index=False)
-    valid.to_csv('../../data/targets/valid.csv', sep=';', index=False)
+    train.to_csv('../../../data/targets/train.csv', sep=';', index=False)
+    test.to_csv('../../../data/targets/test.csv', sep=';', index=False)
+    valid.to_csv('../../../data/targets/valid.csv', sep=';', index=False)
 
 
 if __name__ == '__main__':
