1174c133fe67a91d3447c35e03b6061c7efee704
Mattia Di Fatta <mattia.difatta@mail.polimi.it>
2019-04-23 12:03:27 -0500

    Updated path with real target's and data's path

--- source/tool_shadow/neural_networks/dataset_file_gen.py
+++ source/tool_shadow/neural_networks/dataset_file_gen.py
@@ -3,20 +3,25 @@ from sklearn.utils import shuffle
 
 
 def main():
-    # TODO: change paths
     # read entire csv
-    df = pd.read_csv('../../data/targets/prova.csv', sep=';')
+    df = pd.read_csv('../../data/targets/targets.csv', sep=';')
     # shuffle rows
     df = shuffle(df)
     # count rows
     train_len = int((2 / 3) * len(df.index))
 
+    valid_len = int((3 / 4) * train_len)
+
     # split train and test set
     train, test = df[:train_len], df[train_len:]
 
+    # split validation set
+    train, valid = train[:valid_len], train[valid_len:]
+
     # create new csv files
-    train.to_csv('../../data/targets/prova_train.csv', sep=';', index=False)
-    test.to_csv('../../data/targets/prova_test.csv', sep=';', index=False)
+    train.to_csv('../../data/targets/train.csv', sep=';', index=False)
+    test.to_csv('../../data/targets/test.csv', sep=';', index=False)
+    valid.to_csv('../../data/targets/valid.csv', sep=';', index=False)
 
 
 if __name__ == '__main__':
