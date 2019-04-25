08ed46b1cce5b1ddb50df7e4878061a020edbc1e
Mattia Di Fatta <mattia.difatta@mail.polimi.it>
2019-04-25 11:07:38 -0500

    Added ROC/AUC visualization

--- source/tool_shadow/neural_networks/predictions.py
+++ source/tool_shadow/neural_networks/predictions.py
@@ -1,9 +1,10 @@
 from keras.models import model_from_json
-from tqdm import tqdm
 import cv2
 from matplotlib import pyplot as plt
 import pandas as pd
 import numpy as np
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
 
 
 def main():
@@ -23,23 +24,52 @@ def main():
 
     pred = test[:50]
     x_pred = np.zeros(shape=[50, 240, 320, 3])
-    for i, r in tqdm(pred.iterrows()):
+    for i, r in pred.iterrows():
         r['file'] = '' + test_path + '/' + r['file']
         x_pred[i] = cv2.imread(r['file'])
 
     x_pred = x_pred.reshape([50, 320, 240, 3])
-    print(x_pred.shape)
+
     y_pred = loaded_model.predict_classes(x_pred)
 
     pred['predicted'] = y_pred
+    y_test = []
+    for i, r in pred.iterrows():
+        y_test.append(int(r['valid']))
 
-    print(pred)
+    y_test = np.array(y_test)
 
+    '''
     for _, r in tqdm(pred.iterrows()):
         plt.figure()
         plt.imshow(cv2.cvtColor(cv2.imread(r['file']).astype(np.uint8), cv2.COLOR_BGR2RGB))
         plt.title('ground_truth=%s, predicted=%s' % (r['valid'], r['predicted']))
         plt.show()
+    '''
+
+    fpr_keras, tpr_keras, thresholds_keras = roc_curve(y_test, y_pred)
+
+    auc_keras = auc(fpr_keras, tpr_keras)
+
+    plt.figure()
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr_keras, tpr_keras, label='AUC (area = {:.3f})'.format(auc_keras))
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend(loc='best')
+    plt.show()
+    # Zoom in view of the upper left corner.
+    plt.figure(2)
+    plt.xlim(0, 0.2)
+    plt.ylim(0.8, 1)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr_keras, tpr_keras, label='AUC (area = {:.3f})'.format(auc_keras))
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve (zoomed in at top left)')
+    plt.legend(loc='best')
+    plt.show()
 
 
 if __name__ == '__main__':
