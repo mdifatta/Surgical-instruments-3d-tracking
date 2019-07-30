4cad17b0b7ab4a3fd00285e160e556fcc0bd9fa1
Mattia Di Fatta <mattia.difatta@mail.polimi.it>
2019-07-30 16:00:28 +0200

    Added crop function for testing.

--- source/tool_shadow/kp_tests.py
+++ source/tool_shadow/kp_tests.py
@@ -1,14 +1,20 @@
+import cv2 as cv
 import numpy as np
 from numpy import random
-import cv2 as cv
 
 
 def test_kp():
 
-    img = cv.imread('../../data/datasets/2d_frames_folders/patient1-4/frame3069.png')
+    #img = cv.imread('../../data/datasets/2d_frames_folders/patient2-2/frame4808.png')
+    cam = cv.VideoCapture("../../data/videos/clip-pat-2-video-2.mp4")
+    cam.set(cv.CAP_PROP_FPS, 30)
+    cam.set(cv.CAP_PROP_POS_FRAMES, 1260)
+    _, img = cam.read()
 
     cv.imshow('raw', img)
     cv.waitKey()
+
+    img = crop(img[:1030, :, :])
 
     mask = np.zeros(shape=(img.shape[0], img.shape[1]), dtype=np.uint8)
     #mask[100:150, 120:160] = 255
@@ -33,7 +39,7 @@ def test_kp():
 
     orb = np.copy(img)
 
-    ORB = cv.ORB_create()
+    ORB = cv.ORB_create(nfeatures=20)
     kp = ORB.detect(orb, mask=mask)
     kp, des = ORB.compute(orb, kp)
 
@@ -77,6 +83,20 @@ def test_kp():
 
     cv.imshow('sift - n=%d - Showing best 5 points' % len(kp), sift)
     cv.waitKey()
+
+
+def crop(frame):
+    _, thr = cv.threshold(cv.cvtColor(frame, cv.COLOR_BGR2GRAY),
+                          0,
+                          255,
+                          cv.THRESH_BINARY + cv.THRESH_OTSU
+                         )
+    crop_mask = cv.findNonZero(thr)
+
+    left_edge = crop_mask[:, 0, 0].min()
+    right_edge = crop_mask[:, 0, 0].max()
+
+    return frame[:, left_edge:right_edge, :]
 
 
 if __name__ == '__main__':
