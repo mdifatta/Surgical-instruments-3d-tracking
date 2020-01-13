dc79c18c36d1b9f45b59e71d3ec08e6d0ec9175d
Mattia Di Fatta <mattia.difatta@mail.polimi.it>
2020-01-13 15:10:13 +0100

    Updated script.

--- source/tool_detection/hough_transform.py
+++ source/tool_detection/hough_transform.py
@@ -1,45 +1,36 @@
-import os
-
 import cv2
 from matplotlib import pyplot as plt
-from tqdm import tqdm
 
 from source.tool_detection.utilities import hough_transform
 
-DATA_PATH = "../../data/datasets/frames2d/"
+DATA_PATH = "../../data/videos_2D/"
 OUTPUT_PATH = "../../data/outputs/"
 
 if __name__ == "__main__":
-    image_name = "frame-6.png"
+    filename = "case-3-2D.avi"
 
-    imageNames = []
-    for i in os.listdir(DATA_PATH):
-        imageNames.append(i)
+    cam = cv2.VideoCapture(DATA_PATH + filename)
+    cam.set(cv2.CAP_PROP_POS_FRAMES, 2000)
 
-    id = 0
-    for i in tqdm(imageNames):
-
-        # load image
-        img = cv2.imread(os.path.join(DATA_PATH, i))
+    while cam.isOpened():
+        _ret, full_frame = cam.read()
 
         plt.figure()
-        plt.title('image_rgb %d' % id)
-        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.title('image_rgb')
+        plt.imshow(cv2.cvtColor(full_frame, cv2.COLOR_BGR2RGB))
 
-        _, thr = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 20, 255, cv2.THRESH_BINARY)
+        _, thr = cv2.threshold(cv2.cvtColor(full_frame, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         plt.figure()
         plt.title('mask')
         plt.imshow(thr, 'gray')
         plt.show()
 
-        plot_title = 'rgb_prob_B_%d' % id
+        plot_title = 'rgb_prob_B'
 
-        hough_transform(img,
-                        img,
+        hough_transform(full_frame,
+                        full_frame,
                         probabilistic=True,
                         save=False,
                         title=plot_title,
                         line_clr=(0, 255, 0)
                         )
-
-        id += 1
